9efede97f1271592f22c400215d2df0525fc2dc7
Luca Cilfone - FareilWeb.com <info@fareilweb.com>
2018-01-30 22:57:23 +0100

    First Commit

---
+++ atom-backup.php
@@ -0,0 +1,113 @@
+<?php
+// Get an instance of the class
+$atomBackup = new AtomBackup();
+
+// Elaborate Command Line Argoument
+if($argc === 1) {
+  $atomBackup->printUsage();
+} else {
+
+  switch ($argv[1]) {
+    case '--backup':
+      $atomBackup->makeAtomBackup();
+      break;
+    case '--restore':
+      $atomBackup->restoreAtomBackup();
+      break;
+    default:
+      // nothing...
+      break;
+  }
+}
+
+
+
+/**
+ * [AtomBackup Class]
+ */
+class AtomBackup
+{
+
+  public $user_home = "";
+  public $atom_dot_dir = "";
+  public $atom_files = [];
+  public $backup_folder = "";
+
+  function __construct() {
+    $this->user_home = $_SERVER['HOME'];
+    $this->atom_dot_dir = $this->user_home . DIRECTORY_SEPARATOR . ".atom";
+    $this->backup_folder = dirname(__FILE__) . DIRECTORY_SEPARATOR ."backup";
+    $this->atom_files = [
+      "config.cson",
+      "github.cson",
+      "init.coffee",
+      "keymap.cson",
+      "projects.cson",
+      "snippets.cson",
+      "styles.less"
+    ];
+  }
+
+
+  public function printUsage() {
+    echo
+      "\n|----------- Atom Backup Usage -----------" .
+      "\n| - Make backup:" .
+      "\n|     $ php atom-backup.php --backup" .
+      "\n| - Restore backup:" .
+      "\n|     $ php atom-backup.php --restore" .
+      "\n|-----------------------------------------\n";
+  }
+
+
+  public function makeAtomBackup()
+  {
+    // Store Package List into a File
+    if(file_exists($this->atom_dot_dir)) {
+      $package_list = shell_exec("apm list --installed --bare");
+      file_put_contents($this->backup_folder . DIRECTORY_SEPARATOR . "atom-package-list.txt", $package_list);
+    } else {
+      echo ".atom folder does not exists\n";
+    }
+
+    // Store a Copy of Important Configuration Files
+    foreach ($this->atom_files as $file_name) {
+      $source_file = $this->atom_dot_dir . DIRECTORY_SEPARATOR . $file_name;
+      $dest_file = $this->backup_folder . DIRECTORY_SEPARATOR .  $file_name;
+      copy($source_file, $dest_file);
+    }
+  }
+
+  public function restoreAtomBackup()
+  {
+    // Store Package List into a File
+    if(is_dir($this->atom_dot_dir))
+    {
+      $packages_array = file($this->backup_folder . DIRECTORY_SEPARATOR . "atom-package-list.txt");
+
+      foreach ($packages_array as $package_with_version) {
+          $at_pos = strpos($package_name_with_version, "@");
+          $package_name = "";
+          if($at_pos !== FALSE) {
+            $package_name = substr($package_name_with_version, 0, $at_pos);
+          } else {
+            $package_name = $package_name_with_version;
+          }
+          $package_dir = $this->atom_dot_dir . DIRECTORY_SEPARATOR . $package_name;
+          if(!file_exists($package_dir)) {
+              shell_exec("apm install " . $package_name);
+          }
+      }
+
+    } else {
+      echo ".atom folder does not exists\n";
+    }
+
+    // Restore a Copy of Important Configuration Files
+    foreach ($this->atom_files as $file_name) {
+      $source_file = $this->backup_folder . DIRECTORY_SEPARATOR . $file_name;
+      $dest_file = $this->atom_dot_dir . DIRECTORY_SEPARATOR .  $file_name;
+      copy($source_file, $dest_file);
+    }
+  }
+}
---
+++ readme.md
@@ -0,0 +1,6 @@
+|----------- Atom Backup Usage -----------
+| - Make backup:
+|     $ php atom-backup.php --backup
+| - Restore backup:
+|     $ php atom-backup.php --restore
+|-----------------------------------------
